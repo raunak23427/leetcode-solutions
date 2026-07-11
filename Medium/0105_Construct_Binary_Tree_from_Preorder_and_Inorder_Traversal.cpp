5c2faae3762fc708c9ef4242bdb650a40199f3ea
raunak23427 <162976821+raunak23427@users.noreply.github.com>
2026-07-11 10:23:52 +0530

    Sync LeetCode solution: Construct Binary Tree from Preorder and Inorder Traversal (C++)

---
+++ Medium/0105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.cpp
@@ -0,0 +1,51 @@
+/*
+ * Problem: Construct Binary Tree from Preorder and Inorder Traversal
+ * Problem ID: 105
+ * Difficulty: Medium
+ * Language: C++
+ * Runtime: 8 ms
+ * Memory: 27.2 MB
+ * Synced From: LeetCode
+ * Date: 2026-07-11
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+int find(vector<int>&inorder,int target,int start,int end){
+    for(int i=start;i<=end;i++){
+        if(inorder[i]==target){
+            return i;
+        }
+    }
+    return -1;
+}
+TreeNode * tree(vector<int>&preorder,vector<int>&inorder,int start,int end,int &index){
+    if(start>end){
+        return nullptr;
+    }
+    TreeNode *temp=new TreeNode(preorder[index]);
+    int pos=find(inorder,preorder[index],start,end);
+    index++;
+    temp->left=tree(preorder,inorder,start,pos-1,index);
+    temp->right=tree(preorder,inorder,pos+1,end,index);
+    return temp;
+}
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        int start=0;
+        int end =inorder.size()-1;
+        int index=0;
+        TreeNode* root =tree(preorder,inorder,start,end,index);
+        return root;
+    }
+};
