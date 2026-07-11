5b5447a305e59b17dd99d8164aa950ca74b03683
raunak23427 <162976821+raunak23427@users.noreply.github.com>
2026-07-11 10:26:59 +0530

    Sync LeetCode solution: Construct Binary Tree from Preorder and Inorder Traversal (C++)

--- Medium/0105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.cpp
+++ Medium/0105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.cpp
@@ -3,8 +3,8 @@
  * Problem ID: 105
  * Difficulty: Medium
  * Language: C++
- * Runtime: 8 ms
- * Memory: 27.2 MB
+ * Runtime: 4 ms
+ * Memory: 27 MB
  * Synced From: LeetCode
  * Date: 2026-07-11
  */
@@ -17,35 +17,56 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right)
+ *         : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
-int find(vector<int>&inorder,int target,int start,int end){
-    for(int i=start;i<=end;i++){
-        if(inorder[i]==target){
-            return i;
+
+    int findPos(vector<int>& inorder, int target, int start, int end) {
+        for (int i = start; i <= end; i++) {
+            if (inorder[i] == target)
+                return i;
         }
+        return -1;
     }
-    return -1;
-}
-TreeNode * tree(vector<int>&preorder,vector<int>&inorder,int start,int end,int &index){
-    if(start>end){
-        return nullptr;
+
+    TreeNode* tree(vector<int>& preorder,
+                   vector<int>& inorder,
+                   int start,
+                   int end,
+                   int index) {
+
+        if (start > end)
+            return nullptr;
+
+        TreeNode* temp = new TreeNode(preorder[index]);
+
+        int pos = findPos(inorder, preorder[index], start, end);
+
+        temp->left = tree(preorder,
+                          inorder,
+                          start,
+                          pos - 1,
+                          index + 1);
+
+        temp->right = tree(preorder,
+                           inorder,
+                           pos + 1,
+                           end,
+                           index + (pos - start) + 1);
+
+        return temp;
     }
-    TreeNode *temp=new TreeNode(preorder[index]);
-    int pos=find(inorder,preorder[index],start,end);
-    index++;
-    temp->left=tree(preorder,inorder,start,pos-1,index);
-    temp->right=tree(preorder,inorder,pos+1,end,index);
-    return temp;
-}
+
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        int start=0;
-        int end =inorder.size()-1;
-        int index=0;
-        TreeNode* root =tree(preorder,inorder,start,end,index);
-        return root;
+
+        return tree(preorder,
+                    inorder,
+                    0,
+                    inorder.size() - 1,
+                    0);
     }
 };
