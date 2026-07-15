672e8be619048f736b81b7babec2dfc35432ea44
raunak23427 <162976821+raunak23427@users.noreply.github.com>
2026-07-15 21:50:46 +0530

    Sync LeetCode solution: GCD of Odd and Even Sums (C++)

--- Easy/3995_GCD_of_Odd_and_Even_Sums.cpp
+++ Easy/3995_GCD_of_Odd_and_Even_Sums.cpp
@@ -3,7 +3,7 @@
  * Problem ID: 3995
  * Difficulty: Easy
  * Language: C++
- * Runtime: 3 ms
+ * Runtime: 0 ms
  * Memory: 8.5 MB
  * Synced From: LeetCode
  * Date: 2026-07-15
@@ -12,24 +12,8 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        int sumeven=0;
-        int sumodd=0;
-        int i=1;int m=n;
-        while(n!=0){
-        if(i%2==0){
-           sumeven+=i;
-           n--;
-        }
-        i++;
-        }
-        i=1;
-        while(m!=0){
-        if(i%2!=0){
-           sumodd+=i;
-           m--;
-        }
-        i++;
-        }
+        int sumeven=n*(n+1);
+        int sumodd=n*n;
         
         return __gcd(sumodd, sumeven);
     }
