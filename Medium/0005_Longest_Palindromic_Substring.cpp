7f00ac15d13ae92dd6aeef4a99ef1b556a9c7326
raunak23427 <162976821+raunak23427@users.noreply.github.com>
2026-08-14 10:59:06 +0530

    Sync LeetCode solution: Longest Palindromic Substring (C++)

---
+++ Medium/0005_Longest_Palindromic_Substring.cpp
@@ -0,0 +1,44 @@
+/*
+ * Problem: Longest Palindromic Substring
+ * Problem ID: 5
+ * Difficulty: Medium
+ * Language: C++
+ * Runtime: 11 ms
+ * Memory: 9.3 MB
+ * Synced From: LeetCode
+ * Date: 2026-08-14
+ */
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int start = 0;
+        int maxLen = 1;
+        
+        for(int i = 0; i < s.size(); i++) {
+            
+            int l = i, r = i;
+            while(l >= 0 && r < s.size() && s[l] == s[r]) {
+                if(r - l + 1 > maxLen) {
+                    start = l;
+                    maxLen = r - l + 1;
+                }
+                l--;
+                r++;
+            }
+            
+            l = i;
+            r = i + 1;
+            while(l >= 0 && r < s.size() && s[l] == s[r]) {
+                if(r - l + 1 > maxLen) {
+                    start = l;
+                    maxLen = r - l + 1;
+                }
+                l--;
+                r++;
+            }
+        }
+        
+        return s.substr(start, maxLen);
+    }
+};
