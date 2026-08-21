6801e501605a134481b39d211e372304e62a83a5
raunak23427 <162976821+raunak23427@users.noreply.github.com>
2026-08-21 10:57:56 +0530

    Sync LeetCode solution: Longest Substring Without Repeating Characters (C++)

--- Medium/0003_Longest_Substring_Without_Repeating_Characters.cpp
+++ Medium/0003_Longest_Substring_Without_Repeating_Characters.cpp
@@ -3,8 +3,8 @@
  * Problem ID: 3
  * Difficulty: Medium
  * Language: C++
- * Runtime: 15 ms
- * Memory: 19 MB
+ * Runtime: 5 ms
+ * Memory: 19.1 MB
  * Synced From: LeetCode
  * Date: 2026-08-21
  */
