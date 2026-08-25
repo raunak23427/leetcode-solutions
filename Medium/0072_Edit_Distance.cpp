e4e137b2f9aec2c1db56c89b48550906d9bd8015
raunak23427 <162976821+raunak23427@users.noreply.github.com>
2026-08-25 11:49:48 +0530

    Sync LeetCode solution: Edit Distance (C++)

---
+++ Medium/0072_Edit_Distance.cpp
@@ -0,0 +1,47 @@
+/*
+ * Problem: Edit Distance
+ * Problem ID: 72
+ * Difficulty: Medium
+ * Language: C++
+ * Runtime: 8 ms
+ * Memory: 13.3 MB
+ * Synced From: LeetCode
+ * Date: 2026-08-25
+ */
+
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
+
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1));
+
+        // Base cases
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = i;
+        }
+
+        for (int j = 0; j <= m; j++) {
+            dp[0][j] = j;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+
+                if (word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                else {
+                    dp[i][j] = 1 + min({
+                        dp[i][j - 1],     // insert
+                        dp[i - 1][j],     // delete
+                        dp[i - 1][j - 1]  // replace
+                    });
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+};
