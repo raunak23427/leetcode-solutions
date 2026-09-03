6343e1e19d4e120bc4d4e9b62caf9de692c915cb
raunak23427 <162976821+raunak23427@users.noreply.github.com>
2026-09-04 00:50:51 +0530

    Sync LeetCode solution: Construct Uniform Parity Array II (C++)

---
+++ Medium/4258_Construct_Uniform_Parity_Array_II.cpp
@@ -0,0 +1,36 @@
+/*
+ * Problem: Construct Uniform Parity Array II
+ * Problem ID: 4258
+ * Difficulty: Medium
+ * Language: C++
+ * Runtime: 0 ms
+ * Memory: 165.8 MB
+ * Synced From: LeetCode
+ * Date: 2026-09-03
+ */
+
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        int n = nums1.size();
+        int mn = INT_MAX;
+        
+        for(int i = 0; i < n; i++){
+            if(nums1[i] % 2 != 0){
+                mn = min(mn, nums1[i]);
+            }
+        }
+        if(mn == INT_MAX)
+            return true;
+        
+       
+        for(int i = 0; i < n; i++){
+            if(nums1[i] % 2 == 0){
+                if(mn >= nums1[i])
+                    return false;
+            }
+        }
+        
+        return true;
+    }
+};
