21faa1b872dde50603383d39d257b5549b883231
raunak23427 <162976821+raunak23427@users.noreply.github.com>
2026-09-08 20:45:19 +0530

    Sync LeetCode solution: Make Sum Divisible by P (C++)

--- Medium/1694_Make_Sum_Divisible_by_P.cpp
+++ Medium/1694_Make_Sum_Divisible_by_P.cpp
@@ -3,8 +3,8 @@
  * Problem ID: 1694
  * Difficulty: Medium
  * Language: C++
- * Runtime: 67 ms
- * Memory: 87.7 MB
+ * Runtime: 81 ms
+ * Memory: 87.8 MB
  * Synced From: LeetCode
  * Date: 2026-09-08
  */
