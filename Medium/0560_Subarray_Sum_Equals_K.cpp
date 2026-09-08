4f75b799f7d01aafd3a5feefcb91936b1d4a22c0
raunak23427 <162976821+raunak23427@users.noreply.github.com>
2026-09-08 20:43:46 +0530

    Sync LeetCode solution: Subarray Sum Equals K (C++)

---
+++ Medium/0560_Subarray_Sum_Equals_K.cpp
@@ -0,0 +1,31 @@
+/*
+ * Problem: Subarray Sum Equals K
+ * Problem ID: 560
+ * Difficulty: Medium
+ * Language: C++
+ * Runtime: 32 ms
+ * Memory: 45.6 MB
+ * Synced From: LeetCode
+ * Date: 2026-09-08
+ */
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        mp[0] = 1;
+
+        int sum = 0, ans = 0;
+
+        for (int x : nums) {
+            sum += x;
+
+            if (mp.find(sum - k) != mp.end())
+                ans += mp[sum - k];
+
+            mp[sum]++;
+        }
+
+        return ans;
+    }
+};
